eac2455537ada484b9708fd8efc97284b7cc359b
Josh Marom <josh@josh.co.il>
2022-03-02 00:03:04 +0200

    Improvements to the form

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,49 +1,61 @@
 import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { Formik, Form, Field } from 'formik'
 import * as Yup from 'yup'
-import {Button, Input, Textarea} from '@chakra-ui/react'
+import {Box, Button, FormControl, FormErrorMessage, FormLabel, Heading, Input, Textarea} from '@chakra-ui/react'
 
 const commentFormSchema = Yup.object().shape({
     email: Yup.string().email().required('Please enter your email address'),
     comment: Yup.string().required('You must enter a comment')
 });
 
-function CommentForm() {
-  return (
-      <Formik
-          initialValues={{ email: '', comment: '' }}
-          validationSchema={ commentFormSchema }
-          onSubmit={ ( values, { isSubmitting, resetForm } ) => {
-              fetch('https://jsonplaceholder.typicode.com/comments', {
-                  method: 'POST',
-                  headers: { 'Content-Type': 'application/json' },
-                  body: JSON.stringify(values, null, 2),
-              } ).then( response => response.json() )
-              .then( data => {
-                  console.log('Success:', data);
-                  resetForm();
-              })
-              .catch( error => {
-                  console.log('Error:', error);
-                  isSubmitting = false;
-              });
-          }}
-      >
-          { (formik) => {
-              const { isValid, dirty, isSubmitting } = formik;
-              return (
-              <Form>
-                  <Field type="email" name="email" as={Input} />
-                  <ErrorMessage name="email" component="div" />
-                  <Field name="comment" as={Textarea} />
-                  <ErrorMessage name="comment" component="div" />
-                  <Button as={'button'} type={'submit'} isDisabled={isSubmitting || !(dirty && isValid)}>
-                      Send Comment
-                  </Button>
-              </Form>
-          ) } }
-      </Formik>
-  )
+const sendComment = ( values, { isSubmitting, resetForm } ) => {
+    fetch('https://jsonplaceholder.typicode.com/comments', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(values, null, 2),
+    } )
+        .then( response => response.json() )
+        .then( data => {
+            console.log( 'Success:', data );
+            resetForm();
+        })
+        .catch( error => {
+            console.error( 'Error:', error );
+            isSubmitting = false;
+        });
 }
+
+const CommentForm = () => (
+    <Box p={16} bg={"gray.100"} borderRadius='lg'>
+
+    <Heading as="h4" size="md" mb={4}>Leave a Comment</Heading>
+    <Formik initialValues={{ email: '', comment: '' }} validationSchema={ commentFormSchema }
+          onSubmit={ (values, formik) => { sendComment( values, formik ) } } >
+
+        { (formik) => {
+        const { isValid, dirty, errors , isSubmitting, touched } = formik;
+        return (
+            <Form>
+                <FormControl isInvalid={errors.email && touched.email}>
+                    <FormLabel htmlFor='email'>Email</FormLabel>
+                    <Field type="email" name="email" as={Input} bg='white' />
+                    <FormErrorMessage>{errors.email}</FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={errors.comment && touched.comment}>
+                    <FormLabel htmlFor='comment' mt={4}>Comment</FormLabel>
+                    <Field name="comment" as={Textarea}  bg='white' />
+                    <FormErrorMessage>{errors.comment}</FormErrorMessage>
+                </FormControl>
+
+                <Button as={'button'} mt={4} type={'submit'} colorScheme='blue'
+                        isDisabled={isSubmitting || !(dirty && isValid)}>
+                    Send Comment
+                </Button>
+            </Form>
+        ) } }
+    </Formik>
+    </Box>
+)
 
 export default CommentForm
