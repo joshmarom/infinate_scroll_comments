cd944a253c89998e4bfe35ab38abae386cbba891
Josh Marom <josh@josh.co.il>
2022-03-02 13:27:37 +0200

    Remove unused import

--- src/Comments.js
+++ src/Comments.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { VStack, Box, Spinner } from '@chakra-ui/react'
 import fetchComments from './fetchComments'
 import Comment from './Comment';
